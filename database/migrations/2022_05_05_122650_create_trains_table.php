7c4a8ffc87142fa79ae228c39ed5c29b7b9dc922
GiovanniCalia <johnca@outlook.it>
2022-05-05 14:49:44 +0200

    Creazione repo e preparazione esercizio: crezione tabella "trains"

---
+++ database/migrations/2022_05_05_122650_create_trains_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            /*Azienda
+            Stazione di partenza
+            Stazione di arrivo
+            Orario di partenza
+            Orario di arrivo
+            Codice Treno
+            Numero Carrozze
+            In orario
+            Cancellato*/
+            $table->id();
+            $table->string();
+            $table->string();
+            $table->string();
+            $table->time();
+            $table->time();
+            $table->smallint();
+            $table->tinyint();
+            $table->string();
+            $table->string();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
