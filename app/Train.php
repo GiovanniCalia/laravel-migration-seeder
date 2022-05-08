a7adfd150e58c763dfcdf4276f28d9572f87b3a1
GiovanniCalia <johnca@outlook.it>
2022-05-08 15:12:03 +0200

    Creazione database tramite classe Faker

--- app/Train.php
+++ app/Train.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     //
+    protected $fillable = [
+        'Azienda',
+        'Stazione_di_partenza',
+        'Stazione_di_arrivo',
+        'Orario_di_partenza',
+        'Orario_di_arrivo',
+        'Codice_treno',
+        'Numero_carrozze',
+        'In_orario',
+        'Cancellato',
+        'Data_di_partenza',
+    ];
     public $timestamps = false;
 }
--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Train;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
@@ -9,8 +11,25 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 1; $i < 100; $i++) {
+            $trainData = [
+                'Azienda'               => $faker->word(),
+                'Stazione_di_partenza'  => $faker->city(),
+                'Stazione_di_arrivo'    => $faker->city(),
+                'Orario_di_partenza'    => $faker->time(),
+                'Orario_di_arrivo'      => $faker->time(),
+                'Codice_treno'          => $faker->numberBetween(10000, 99999),
+                'Numero_carrozze'       => $faker->numberBetween(3, 50),
+                'In_orario'             => $faker->boolean(),
+                'Cancellato'            => $faker->boolean(),
+                'Data_di_partenza'      => $faker->date(),
+            ];
+
+            $train = new Train();
+            $train->fill($trainData);
+            $train->save();
+        }
     }
 }
